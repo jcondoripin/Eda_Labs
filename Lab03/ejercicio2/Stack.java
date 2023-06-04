f63ca961b290a0a4afc7299ba5272ae01de1375c
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-03 20:02:29 -0500

    Metodos de contenido, tamaño y verificado de contenido

--- Lab03/ejercicio2/Stack.java
+++ Lab03/ejercicio2/Stack.java
@@ -8,5 +8,20 @@ public class Stack {
         top = null;
         size = 0;
     }
+    //Verificar si la pila esta vacia
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    //Tamaño de la pila
+    public int size() {
+        return size;
+    }
+    //Insertar un dato en la pila
+    public void push(T data) {
+        Node<T> newNode = new Node<>(data);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
 
 }
