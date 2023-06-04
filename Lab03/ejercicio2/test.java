28194340061802a7121b7f925389597fe6e87bd7
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-03 20:32:50 -0500

    Probando los metodos que creamos

--- Lab03/ejercicio2/test.java
+++ Lab03/ejercicio2/test.java
@@ -1,5 +1,16 @@
-public class test{
-    public static void main (String [] args){
-        
+package ejercicio2;
+
+public class test {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+        System.out
+                .println("Tamaño: " + stack.size() + "\n" + "El elemento superior y que se quitara es: " + stack.pop());
+        int indicePrueba = stack.search("B");
+        System.out.println("El elemento B, esta en la posicion: " + indicePrueba);
+        System.out.println("Tamaño: " + stack.size());
+        System.out.println("Elemento superior: " + stack.peek());
     }
 }
