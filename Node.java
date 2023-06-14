21e1099f756968d3e12fed927b1115f8871beddc
JoseGordilloMendoza <121585572+JoseGordilloMendoza@users.noreply.github.com>
2023-06-14 13:49:27 -0500

    Clase nodo, lista enlazada y script de gnuplot

---
+++ Node.java
@@ -0,0 +1,30 @@
+package question1;
+
+public class Node <T>{
+    private T valor;
+    private Node next;
+
+    public T getValor() {
+        return valor;
+    }
+
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node(T valor) {
+        this.valor = valor;
+    }
+
+    public Node(T valor, Node next) {
+        this.valor = valor;
+        this.next = next;
+    }
+}
---
+++ NodeList.java
@@ -0,0 +1,22 @@
+package question1;
+
+public class NodeList<T> {
+    private Node<T> raiz;
+    private int size = 0;
+
+    public Node<T> getRaiz() {
+    return raiz;
+    }
+
+    public void setRaiz(Node<T> raiz) {
+    this.raiz = raiz;
+    }
+
+    public int getSize() {
+    return size;
+    }
+
+    public void setSize(int size) {
+    this.size = size;
+    }
+}
