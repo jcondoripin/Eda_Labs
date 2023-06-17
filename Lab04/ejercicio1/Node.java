1b96c30c508b1ca656ed0c9289857ae42d8a34cd
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-06-17 17:56:39 -0500

    clase Node con metodos

--- Lab04/ejercicio1/Node.java
+++ Lab04/ejercicio1/Node.java
@@ -1,5 +1,28 @@
 package Lab04.ejercicio1;
 
-public class Node {
+public class Node <T> {
+    private T valor;
+    private Node next;
     
+    public T getValor() {
+    return valor;
+    }
+
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    public Node(T valor) {
+        this.valor = valor;
+    }
+    public Node(T valor, Node next) {
+        this.valor = valor;
+        this.next = next;
+    }
 }
