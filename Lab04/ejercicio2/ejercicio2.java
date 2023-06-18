8daa483933ba3984bff4a2b6998ec66c6d42fc4c
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-17 23:39:18 -0500

    Probando metodo de insercion

--- Lab04/ejercicio2/ejercicio2.java
+++ Lab04/ejercicio2/ejercicio2.java
@@ -2,4 +2,27 @@ public class ejercicio2 {
     public void main(String[] args) {
 
     }
+
+    public void insertionSort(DoubleNodeList<Integer> list) {
+        Node<Integer> puntero = list.getHead().getNextNode();
+
+        while (puntero != null) {
+            Node<Integer> keyNode = puntero;
+            int key = keyNode.getData();
+            Node<Integer> prevNode = puntero.getPreviuosNode();
+
+            while (prevNode != null && prevNode.getData().compareTo(key) > 0) {
+                prevNode.getNextNode().setData(prevNode.getData());
+                prevNode = prevNode.getPreviuosNode();
+            }
+
+            if (prevNode == null) {
+                list.getHead().setData(key);
+            } else {
+                prevNode.getNextNode().setData(key);
+            }
+            puntero = puntero.getNextNode();
+        }
+    }
+
 }
