dfd0179af4a58ce855ef49a3385d2e3b989116c6
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-18 00:03:27 -0500

    Main construido y arreglos en insertionSort y peoresCasos

--- Lab04/ejercicio2/ejercicio2.java
+++ Lab04/ejercicio2/ejercicio2.java
@@ -1,11 +1,33 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class ejercicio2 {
-    public void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa el tamaño maximo: ");
+        int tamano = sc.nextInt();
+        ArrayList<DoubleNodeList<Integer>> casos = new ArrayList<>();
+        for (int n = 1; n <= tamano; n++) {
+            casos.add(peorCaso(n));
+        }
+        PrintWriter archivo = new PrintWriter("insertionTimes.txt");
+
+        Iterator<DoubleNodeList<Integer>> puntero = casos.iterator();
+        while (puntero.hasNext()) {
+            archivo.println(String.valueOf(insertionSort(puntero.next())));
+        }
+        archivo.close();
+        sc.close();
 
     }
 
-    public void insertionSort(DoubleNodeList<Integer> list) {
+    public static long insertionSort(DoubleNodeList<Integer> list) {
         Node<Integer> puntero = list.getHead().getNextNode();
-
+        long start = System.nanoTime();
         while (puntero != null) {
             Node<Integer> keyNode = puntero;
             int key = keyNode.getData();
@@ -23,12 +45,16 @@ public class ejercicio2 {
             }
             puntero = puntero.getNextNode();
         }
+        long end = System.nanoTime();
+        return end - start;
     }
 
-    public static void peorCaso(int t, DoubleNodeList<Integer> lista) {
+    public static DoubleNodeList<Integer> peorCaso(int t) {
+        DoubleNodeList<Integer> lista = new DoubleNodeList<>();
         for (int i = t; i >= 0; i--) {
             lista.addNode(i);
         }
+        return lista;
     }
 
 }
