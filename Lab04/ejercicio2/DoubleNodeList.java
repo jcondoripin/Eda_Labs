33308bc32c9505a48533a71b717fcb5805e2ccd4
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-17 23:02:08 -0500

    Creacion metodo remove

---
+++ Lab04/ejercicio2/DoubleNodeList.java
@@ -0,0 +1,37 @@
+public class DoubleNodeList<E> {
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    public DoubleNodeList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void remove(E data) {
+        Node<E> puntero = head;
+        while (puntero != null) {
+            if (puntero.getData().equals(data)) {
+                if (puntero == head) {
+                    head = puntero.getNextNode();
+                    if (head != null) {
+                        head.setPreviousNode(null);
+                    } else {
+                        tail = null;
+                    }
+                } else if (puntero == tail) {
+                    tail = puntero.getPreviuosNode();
+                    tail.setNextNode(null);
+                } else {
+                    Node<E> previousNode = puntero.getPreviuosNode();
+                    Node<E> nextNode = puntero.getNextNode();
+                    previousNode.setNextNode(nextNode);
+                    nextNode.setPreviousNode(previousNode);
+                }
+                break;
+            }
+            puntero = puntero.getNextNode();
+        }
+    }
+}
