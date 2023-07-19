f4448e89a6faca36b8ff3a048d9d07d4d765ef3a
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-07-19 13:42:57 -0500

    Metodo buscar comentado

--- Lab07/Trie.java
+++ Lab07/Trie.java
@@ -15,7 +15,16 @@ public class Trie {
 
     // Buscar si una palabra existe en el Trie
     public boolean search(String palabra) {
-        return false;
+        TrieNode current = root; //Este será nuestro puntero
+        for (char c : palabra.toCharArray()) /*Recorre la palabra descompuesta por el CharArray*/{
+            int index = c - 'a'; //Calculando indice, restando valores ASCII 
+            if (current.children[index] == null) {
+                return false; //Si hay un nodo que no coincide, la palabra no eiste
+            }
+            current = current.children[index];//Seguimos recorriendo el trie
+        }
+        return current.isEndOfWord;/*En caso de que la ultima letra/nodo si sea termino de palabra, se encontró la palabra 
+        Si después de recorrer todos los caracteres de la palabra no llegamos a un nodo final, no se encontro la palabra*/
     }
 
     // Reemplazar una palabra vieja por otra nueva
