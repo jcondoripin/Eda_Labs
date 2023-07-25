bc4d2ec04ce5ab5bc1b101f976c630efc59be373
Ower Frank <121266700+OwerFrankLopezArela@users.noreply.github.com>
2023-07-25 00:25:00 -0500

    Se establece un mecanismo para escuchar y manejar eventos de acción en el botón "jButton1"
    Cuando se produzca un evento de acción en el botón (por ejemplo, cuando el usuario haga clic en él), se invocará el método "jButton1ActionPerformed" para que se pueda realizar acciones específicas en respuesta al evento. La implementación real de las acciones específicas ya se definio en el método "jButton1ActionPerformed".

--- Lab07/Ventana.java
+++ Lab07/Ventana.java
@@ -63,7 +63,13 @@ public class Ventana extends javax.swing.JFrame {
         jButton1.setBackground(new java.awt.Color(187, 187, 187));
         jButton1.setForeground(new java.awt.Color(65, 65, 65));
         jButton1.setText("Insert");
-
+        // Agrega un ActionListener al JButton (botón) llamado "jButton1"
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        
         javax.swing.GroupLayout InsertLayout = new javax.swing.GroupLayout(Insert);
         Insert.setLayout(InsertLayout);
         InsertLayout.setHorizontalGroup(
