1c6670334f5d825f2757c71f99de2236d192d703
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-07-25 20:05:25 -0500

    Interfaz simple

---
+++ Lab07/GUITrie.java
@@ -0,0 +1,60 @@
+package Lab07;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUITrie extends JFrame {
+    private final int ALTO = 600;
+    private final int ANCHO = 600;
+
+    public GUITrie() {
+        this.setTitle("Trie");
+        this.setSize(ANCHO, ALTO);
+        this.setLayout(new BorderLayout(ALLBITS, ABORT));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(Color.BLUE);
+        this.createContents();
+        this.setVisible(true);
+    }
+
+    public void createContents() {
+        Trie trie = new Trie();
+        getContentPane().setBackground(Color.LIGHT_GRAY);
+        JMenuBar mb = new JMenuBar();
+        JMenu replace = new JMenu("REEMPLAZAR");
+        mb.add(replace);
+
+        JTextArea areaTexto = new JTextArea();
+
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Introducir texto");
+        JTextField tf = new JTextField(10);
+        JButton insertWord = new JButton("Insertar palabra");
+        JButton insertText = new JButton("Insertar texto");
+        JButton buscar = new JButton("Buscar");
+        panel.add(label);
+        panel.add(tf);
+        panel.add(insertWord);
+        panel.add(insertText);
+        panel.add(buscar);
+
+        // Agregar componentes al marco.
+        this.add(BorderLayout.NORTH, mb);
+        this.add(BorderLayout.CENTER, areaTexto);
+        this.add(BorderLayout.SOUTH, panel);
+
+        insertWord.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                trie.insert(tf.getText());
+                System.out.println("Palabra insertada en el Trie");
+                areaTexto.setText(trie.toString());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new GUITrie();
+    }
+}
