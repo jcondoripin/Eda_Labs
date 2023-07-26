04cdde2c8e225404631398ffd9e326dead425f52
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-07-25 20:03:54 -0500

    agregando metodos  accesoses de isEndOfWord

--- Lab07/TrieNode.java
+++ Lab07/TrieNode.java
@@ -5,10 +5,18 @@ public class TrieNode {
     final int ALPHABET_SIZE = 26;
 
     TrieNode[] children;
-    boolean isEndOfWord;
-    
+    private boolean isEndOfWord;
+
     public TrieNode() {
         this.children = new TrieNode[ALPHABET_SIZE];
+    }
+
+    public boolean isEndOfWord() {
+        return isEndOfWord;
+    }
+
+    public void setIsEndOfWord(boolean a) {
+        this.isEndOfWord = a;
     }
 
 }
