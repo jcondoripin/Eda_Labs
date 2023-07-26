2f6b415040d07dc74778279558a58fcfde527deb
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-07-25 20:54:31 -0500

    Funcionalidad de search y agregados modos de replace y el normal

--- Lab07/GUITrie.java
+++ Lab07/GUITrie.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 
 public class GUITrie extends JFrame {
     private final int ALTO = 600;
-    private final int ANCHO = 600;
+    private final int ANCHO = 1000;
 
     public GUITrie() {
         this.setTitle("Trie");
@@ -23,27 +23,74 @@ public class GUITrie extends JFrame {
         Trie trie = new Trie();
         getContentPane().setBackground(Color.LIGHT_GRAY);
         JMenuBar mb = new JMenuBar();
-        JMenu replace = new JMenu("REEMPLAZAR");
+        JButton replace = new JButton("MODO REPLACE");
+        JButton home = new JButton("FIND AND INSERT");
         mb.add(replace);
+        mb.add(home);
 
         JTextArea areaTexto = new JTextArea();
 
         JPanel panel = new JPanel();
         JLabel label = new JLabel("Introducir texto");
+        JLabel label2 = new JLabel("Palabra a reemplazar");
+        label2.setVisible(false);
         JTextField tf = new JTextField(10);
+        JTextField tf2 = new JTextField(10);
+        tf2.setVisible(false);
         JButton insertWord = new JButton("Insertar palabra");
         JButton insertText = new JButton("Insertar texto");
         JButton buscar = new JButton("Buscar");
+        JLabel searchLabel = new JLabel("");
+        JButton reemplazar = new JButton("Reemplazar");
+        reemplazar.setVisible(false);
         panel.add(label);
         panel.add(tf);
         panel.add(insertWord);
         panel.add(insertText);
         panel.add(buscar);
+        panel.add(label2);
+        panel.add(tf2);
+        panel.add(reemplazar);
+        panel.add(searchLabel);
 
         // Agregar componentes al marco.
         this.add(BorderLayout.NORTH, mb);
         this.add(BorderLayout.CENTER, areaTexto);
         this.add(BorderLayout.SOUTH, panel);
+        buscar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (trie.search(tf.getText())) {
+                    searchLabel.setText("La palabra \' " + tf.getText() + "\' está presente");
+                    tf.setText("");
+                } else {
+                    searchLabel.setText("La palabra \' " + tf.getText() + "\' NO está presente");
+                    tf.setText("");
+                }
+            }
+        });
+
+        replace.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                label.setText("Palabra nueva: ");
+                label2.setVisible(true);
+                tf2.setVisible(true);
+                reemplazar.setVisible(true);
+                insertWord.setVisible(false);
+                insertText.setVisible(false);
+                buscar.setVisible(false);
+            }
+        });
+        home.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                label.setText("Introducir texto ");
+                label2.setVisible(false);
+                tf2.setVisible(false);
+                reemplazar.setVisible(false);
+                insertWord.setVisible(true);
+                insertText.setVisible(true);
+                buscar.setVisible(true);
+            }
+        });
 
         insertWord.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
